a49535be33570aed613de66122dfe73b452b0aab
silicagel <ksk19981004@gmail.com>
2024-12-18 21:30:27 +0900

    Initial Commit

---
+++ LifeCompass/LifeCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCompassApp.swift
+//  LifeCompass
+//
+//  Created by silicagel on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct LifeCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
